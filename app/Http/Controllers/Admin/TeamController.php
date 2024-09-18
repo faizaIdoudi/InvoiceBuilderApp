fc5b9382388144e5732f0fce6abd54c9d93b02b7
Wejden-Chledi <wejdenchledi4@gmail.com>
2024-09-18 13:06:17 +0100

    add-team

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -7,39 +7,37 @@ use App\Models\Team;
 
 class TeamController extends Controller
 {
-
-    function index(){
-        $team = Team::orderBy('id','desc')->get();
-        return inertia('Team/Index' ,compact('team'));
+    public function index() {
+        $teams = Team::orderBy('id', 'desc')->get();
+        return inertia('Team/Index', ['teams' => $teams]);
     }
-
+    
     public function show(Team $team) {
         return inertia('Team/show',compact('team'));
     }
 
     public function create(){
         return inertia('Team/Create');
-     }
+    }
 
-     public function store(Request $request)
-{
-    $request->validate([
-        'first_name' => 'required|string|max:255',
-        'surname' => 'required|string|max:255',
-        'email' => 'required|email|unique:teams,email',
-        'password' => 'required|string|min:8',
-    ]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'email' => 'required|email|unique:teams,email',
+            'password' => 'required|string|min:8',
+        ]);
 
-    $team = new Team();
-    $team->first_name = $request->first_name;
-    $team->surname = $request->surname;
-    $team->email = $request->email;
-    $team->password = bcrypt($request->password);
-    $team->save();
+        $team = new Team();
+        $team->first_name = $request->first_name;
+        $team->surname = $request->surname;
+        $team->email = $request->email;
+        $team->password = bcrypt($request->password);
+        $team->save();
 
-    return redirect()->route('team.index');
-}
-
+        return redirect()->route('team.index');
+    }
 
       public function edit($slug) {
         $team = Team::where('slug',$slug)->firstOrFail();
